ab3147acc7a35fb942c1a1f25d82ff7598e60cf3
kashikumar <kashinath.intern@gmail.com>
2022-07-21 21:12:52 +0530

    clear bugs

--- src/component2/SearchImg.jsx
+++ src/component2/SearchImg.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 export const SearchImg = () => {
     const [SearchImg, setSearchImg] = useState()
